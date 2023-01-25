3009954396295dbc310ba87cdcdc96351f930aa4
amirahnasihah <amirahnasihah97@gmail.com>
2023-01-25 23:25:03 +0800

    todo

---
+++ components/whatido.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const WhatIDo = () => {
+  const todo = [
+    {
+      id: 1,
+      adverb: "Building 💻",
+      body: "I keep building products and learning to make one. It is always my passion and something I do every day",
+    },
+    {
+      id: 2,
+      adverb: "Blogging ✍️",
+      body: "I document my tech journey and learning progress. It has inspired many people to join the tech industry",
+    },
+    {
+      id: 3,
+      adverb: "Sharing 🌏",
+      body: "I share high-quality content, lessons and insights so everyone can take benefit from them",
+    },
+    {
+      id: 4,
+      adverb: "Designing 🎨",
+      body: "I innovate design to fulfill the aesthetic requirements of the front-end. A mix of tech and design",
+    },
+  ];
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default WhatIDo;
